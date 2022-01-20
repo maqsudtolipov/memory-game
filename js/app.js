2df34e3bcacaf2642f88cafbc0b6bb8685837e3a
Maqsud <maqsudtolipov9@gmail.com>
2022-01-21 01:51:24 +0500

    Create cards once again with js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,79 @@
+'use strict';
+// memory game
+
+const gameData = [
+  {
+    name: 'rose',
+  },
+  {
+    name: 'rose',
+  },
+  {
+    name: 'heart',
+  },
+  {
+    name: 'heart',
+  },
+  {
+    name: 'game-controller',
+  },
+  {
+    name: 'game-controller',
+  },
+  {
+    name: 'balloon',
+  },
+  {
+    name: 'balloon',
+  },
+  {
+    name: 'bulb',
+  },
+  {
+    name: 'bulb',
+  },
+  {
+    name: 'color-palette',
+  },
+  {
+    name: 'color-palette',
+  },
+  {
+    name: 'hammer',
+  },
+  {
+    name: 'hammer',
+  },
+  {
+    name: 'snow',
+  },
+  {
+    name: 'snow',
+  },
+];
+
+const grid = document.getElementById('grid');
+
+// create game board
+const createBoard = function () {
+  for (let i = 0; i < gameData.length; i++) {
+    const card = `<div class="card"><ion-icon name="${gameData[i].name}"></ion-icon></div>`;
+    grid.insertAdjacentHTML('beforeend', card);
+    // grid[i].addEventListener('click', flipCard);
+  }
+};
+
+createBoard();
+
 const cardsEls = document.querySelectorAll('.card');
 
-cardsEls.forEach(card => {
+cardsEls.forEach((card) => {
   card.addEventListener('click', () => {
     card.classList.toggle('card--flip');
-    console.log(card);
   });
 });
+
+// flip card
+// function flipCard() {
+//   console.log('flip card');
+// }
